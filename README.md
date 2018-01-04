3673d8f7191b063ccc4e540cd7ad9ef90692710f
ChanceYu <i.fish@foxmail.com>
2018-01-05 00:24:40 +0800

    :sleeping:

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# wxgame-snaker
+## wxgame-snaker
 微信小游戏，贪吃蛇
---
+++ js/main.js
@@ -0,0 +1,14 @@
+let ctx   = canvas.getContext('2d')
+
+/**
+ * 游戏主函数
+ */
+export default class Main {
+  constructor() {
+    
+  }
+
+  restart() {
+    
+  }
+}
